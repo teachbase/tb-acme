053d200e6f5e5cb5174bdba0ca51081235f18a4b
Stanislav Kravchenko <stankra@yandex.ru>
2019-10-15 18:06:42 +0300

    Refact resource

--- lib/stages/resource.rb
+++ lib/stages/resource.rb
@@ -1,31 +1,25 @@
 # frozen_string_literal: true
 
-require "ostruct"
-
 module Stages
   class Resource
-    extend Forwardable
+    attr_accessor :client, :account, :order, :challenge, :certificate,
+                  :private_key, :keyword_init, :errors
 
     def initialize(client: , account:)
-      @resource = OpenStruct.new(
-        client:       client,
-        account:      account,
-        order:        nil,
-        challenge:    nil,
-        certificate:  nil,
-        private_key:  nil,
-        keyword_init: true,
-        errors:       []
-      )
+      @client       = client
+      @account      = account
+      @order        = nil
+      @challenge    = nil
+      @certificate  = nil
+      @private_key  = nil
+      @keyword_init = true
+      @errors       = []
 
       error(:account, "Account is nil") if account.nil?
     end
 
-    def_delegators :@resource, :client, :account, :order, :challenge, :certificate,
-                   :private_key, :keyword_init, :errors
-
     def valid?
-      @resource.errors.empty?
+      errors.empty?
     end
 
     def invalid?
@@ -33,7 +27,7 @@ module Stages
     end
 
     def error(field, message)
-      @resource.errors << { field => message }
+      errors << { field => message }
     end
   end
 end
