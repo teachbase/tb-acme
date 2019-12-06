351448dd686140bdc5728da75da8b114c35608af
Stanislav Kravchenko <stankra@yandex.ru>
2019-12-06 13:58:55 +0300

    Remove explicit raven from rake

--- boot.rb
+++ boot.rb
@@ -17,10 +17,5 @@ class Boot
     log_file = "#{Config.settings['remote_path']}/shared/log/stdout"
     $logger  = ::Logger.new(log_file)
     $redis   = ::Redis.new(host: 'localhost', port: 6379, db: 0)
-
-    Raven.configure do |config|
-      config.dsn = Config.settings['raven_dsn']
-      config.environments = ['staging', 'production']
-    end
   end
 end
