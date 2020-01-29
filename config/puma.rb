f3eab9c645daac27d71c1bfca57eaa2a57968f02
Andrey Paderin <paderinandrey2011@gmail.com>
2020-01-30 02:45:13 +0300

    fix puma conf

--- config/puma.rb
+++ config/puma.rb
@@ -10,20 +10,16 @@ threads 1, 4
 env = (ENV['RACK_ENV'] || 'production')
 
 if env == 'production'
-  daemonize
-
   config = YAML.load_file("#{File.dirname(__FILE__)}/secrets.yml").fetch(env, {})
-
   app_dir = File.expand_path("../..", __FILE__)
-  shared_dir = "#{config['remote_path']}/shared"
 
-  stdout_redirect "#{shared_dir}/log/stdout", "#{shared_dir}/log/stderr"
+  stdout_redirect "#{app_dir}/log/stdout", "#{app_dir}/log/stderr"
   environment env
 
-  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-  pidfile "#{shared_dir}/tmp/pids/puma.pid"
-  state_path "#{shared_dir}/tmp/sockets/puma.state"
-  activate_control_app "unix://#{shared_dir}/tmp/sockets/pumactl.sock"
+  bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+  pidfile "#{app_dir}/tmp/pids/puma.pid"
+  state_path "#{app_dir}/tmp/sockets/puma.state"
+  activate_control_app "unix://#{app_dir}/tmp/sockets/pumactl.sock"
 else
   environment "development"
 end
