2b9bef742cc02b791925a265f55f74131a49fcbe
Andrey Paderin <paderinandrey2011@gmail.com>
2020-01-30 20:16:56 +0300

    fix boot app

--- boot.rb
+++ boot.rb
@@ -14,13 +14,12 @@ Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |f| require(f) }
 
 class Boot
   def self.load
-    log_file = "#{Config.settings['remote_path']}/shared/log/stdout"
-    $logger  = ::Logger.new(log_file)
-    $redis   = ::Redis.new(host: 'localhost', port: 6379, db: 0)
-
     Raven.configure do |config|
       config.dsn = Config.settings['raven_dsn']
       config.environments = ['staging', 'production']
     end
+
+    $logger = ::Logger.new(STDOUT)
+    $redis  = ::Redis.new(host: 'localhost', port: 6379, db: 0)
   end
 end
