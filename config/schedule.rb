ed45f27300bda7aaf8a0a54ee77b7cce9e4c08b9
paderinandrey <andy.paderin@gmail.com>
2021-01-22 22:10:21 +0300

    Add env to schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,12 @@
 set :environment, ENV['RACK_ENV']
 set :output, error: '/var/log/error.log', standard: '/var/log/cron.log'
 
+env 'GEM_HOME', ENV['GEM_HOME']
+env 'BUNDLE_PATH', ENV['GEM_HOME']
+env 'BUNDLE_APP_CONFIG', ENV['BUNDLE_PATH']
+env 'BUNDLE_BIN', "#{ENV['BUNDLE_PATH']}/bin"
+env 'PATH', ENV['PATH']
+
 if ENV['APP_ROLE'] == 'master'
   every 1.day, at: '11:00pm' do
     rake 'cert:refresh'
